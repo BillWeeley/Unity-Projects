f3379a6d915f9da04a75be24df9612e8149ee7d5
BillWeeley <61968397+BillWeeley@users.noreply.github.com>
2020-03-09 10:23:09 +0000

    Aids
    mashallah

---
+++ New Unity Project/Assets/Scripts/Text_Script 2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Text_Script2 : MonoBehaviour
+{
+    public GameObject health1, health2, health3;
+    public GameObject playerScore;
+    public static int score = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text_Script2.score = 100;
+        Text txt1 = playerScore.GetComponent<Text>();
+        txt1.text = (score + "100");
+        score--;
+        if (score == 100)
+        {
+            Destroy(health1);
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Text_Script.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Text_Script : MonoBehaviour
+{
+    public GameObject health1, health2, health3;
+    //public GameObject playerScore;
+    public GameObject playerAmmo;
+    public static int ammo = 50;
+    //public static int score = 100;
+      //set the score to 100
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Text txt = playerAmmo.GetComponent<Text>();
+            txt.text = (ammo + "/50");
+            ammo--;
+        }
+
+    
+
+        if (ammo == 0)
+        {
+            ammo = 50;
+        }
+
+        //
+
+
+    }
+}
